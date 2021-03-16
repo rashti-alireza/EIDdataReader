b2f0137bb82b31118af980083c859aa6a653e6b4
Alireza Rashti <rashti.alireza@gmail.com>
2021-03-16 11:38:10 -0400

    Minor.

--- EIDdataReader.h
+++ EIDdataReader.h
@@ -27,7 +27,7 @@
 #define ASSERT assert
 
 /* deleting files created by Elliptica */
-#define DELETE 1
+#define DELETE 0
 
 /* this is how we write binary data: first write size and then value. 
 // thus, when we wanna read the data the first one gives of the memory allocation 
